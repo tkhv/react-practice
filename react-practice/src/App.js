12558e1ae315751cd34e6b4f9d63380033268426
tkhv <harsha.02k@gmail.com>
2022-07-28 17:36:58 -0400

    Added react components for todo cards

---
+++ react-practice/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>Todos:</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
